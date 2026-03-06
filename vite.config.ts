8da4942db21997fd5e7cc4f20bc93dbf1204b392
Iqbal <imaniatfan@gmail.com>
2026-03-06 22:18:50 +0700

    setup deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/",
     resolve: {
         alias: [
             { find: '@', replacement: path.resolve(__dirname, './src') }
